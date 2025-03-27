e35ca1aaadb70a8813648cfbe2a0399e88c97c8f
chillpill128 <130168214+chillpill128@users.noreply.github.com>
2025-03-27 14:36:41 +0300

    Add files via upload

---
+++ blogicum/blog/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Post, Comments
+
+
+class ProfileChangeForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email']\
+
+class PostCreateForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'text', 'pub_date', 'location', 'category', 'image']
+        widgets = {
+            'pub_date': forms.DateInput(attrs={'type': 'date'})
+        }
+
+class CommentsForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        fields = ('text',)
+
